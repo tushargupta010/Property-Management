0f1b508edda73a06c814507dfba8adb4254e5280
tushar <tushargupta010@gmail.com>
2021-11-13 17:19:40 +0530

    Property Management with Airtable

--- script.js
+++ script.js
@@ -7,52 +7,92 @@ let propertiesElement = document.querySelector(".properties");
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
-  let property = new Property(pName.value, pDesc.value, pSize.value);
-  addProperty(property);
+
+  (async () => {
+    const rawResponse = await fetch(
+      "https://api.airtable.com/v0/appzxi82c0Ddnsxio/property",
+      {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer keyUcrNV9TqyJjGhn",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          records: [
+            {
+              fields: {
+                name: pName.value,
+                description: pDesc.value,
+                size: pSize.value,
+              },
+            },
+          ],
+        }),
+      }
+    );
+    const content = await rawResponse.json();
+    console.log(content);
+    console.log(content.records[0]);
+    addProperty(content.records[0]);
+  })();
+  hideForm();
 });
 
-function addProperty(property) {
+function addProperty(record) {
+  let property = record.fields;
   let card = document.createElement("div");
   card.classList.add("card");
 
   if (property.name && property.size) {
     card.innerHTML = `<h4 >Name : ${property.name}</h4>
         <p>Description : ${property.description}</p>
-        <p>Size : ${property.size}</p>
-        <button class="del">Delete</button><br/>`;
+        <p>Size : ${property.size} sq. yards</p>
+        <button class="del" onclick="deleteProperty('${record.id}')">Delete</button><br/>`;
 
     propertiesElement.appendChild(card);
     let clear = card.querySelector(".del");
     clear.addEventListener("click", () => {
       card.remove();
     });
-    document.getElementsByClassName("container")[0].style.display = "none";
-    document.getElementsByClassName("properties")[0].style.display = "initial";
-    document.querySelector("#addPropertyButton").style.display = "initial";
-    document.querySelector("#backButton").style.display = "none";
+    hideForm();
     clearFields();
   }
 }
 
-function clearFields() {
-  pName.value = null;
-  pDesc.value = null;
-  pSize.value = null;
+function deleteProperty(id) {
+  (async () => {
+    const response = await fetch(
+      "https://api.airtable.com/v0/appzxi82c0Ddnsxio/property/" + id,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: "Bearer keyUcrNV9TqyJjGhn",
+        },
+      }
+    );
+  })();
 }
 
 function initApp() {
-  addProperty(
-    new Property("Sagar Apartment", "Canaught Palace, Delhi Central", 2)
-  );
-  addProperty(
-    new Property("Godrej South Estate", "Apartment in Okhla, Delhi South", 3)
-  );
-  addProperty(new Property("Shree Shyam House", "Chattarput, Delhi South", 4));
-  document.getElementsByClassName("container")[0].style.display = "none";
-  document.getElementsByClassName("properties")[0].style.display = "initial";
-  document.querySelector("#addPropertyButton").style.display = "initial";
-  document.querySelector("#backButton").style.display = "none";
+  showProperties();
+  hideForm();
 }
+
+const showProperties = async () => {
+  const response = await fetch(
+    "https://api.airtable.com/v0/appzxi82c0Ddnsxio/property",
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer keyUcrNV9TqyJjGhn",
+      },
+    }
+  );
+  const myJson = await response.json();
+  myJson.records.forEach((element) => {
+    addProperty(element);
+  });
+};
 
 class Property {
   constructor(name, description, size) {
@@ -65,16 +105,30 @@ class Property {
 initApp();
 
 function addPropertyButton() {
-  document.getElementsByClassName("container")[0].style.display = "initial";
-  document.getElementsByClassName("properties")[0].style.display = "none";
-  document.querySelector("#addPropertyButton").style.display = "none";
-  document.querySelector("#backButton").style.display = "initial";
+  showForm();
   clearFields();
 }
 
 function backButton() {
+  hideForm();
+}
+
+function hideForm() {
   document.getElementsByClassName("container")[0].style.display = "none";
   document.getElementsByClassName("properties")[0].style.display = "initial";
   document.querySelector("#addPropertyButton").style.display = "initial";
   document.querySelector("#backButton").style.display = "none";
+}
+
+function showForm() {
+  document.getElementsByClassName("container")[0].style.display = "initial";
+  document.getElementsByClassName("properties")[0].style.display = "none";
+  document.querySelector("#addPropertyButton").style.display = "none";
+  document.querySelector("#backButton").style.display = "initial";
+}
+
+function clearFields() {
+  pName.value = null;
+  pDesc.value = null;
+  pSize.value = null;
 }
